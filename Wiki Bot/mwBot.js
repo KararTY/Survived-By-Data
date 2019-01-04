b5a8fae2a7adce0413911fb76805a26118739c8b
Karar Al-Remahy <alremahykarar@ymail.com>
2019-01-04 03:16:59 +0100

    MediaWiki bot update * "Generation string" is now added before appending to wiki: Based on last git commit date.

--- Wiki Bot/mwBot.js
+++ Wiki Bot/mwBot.js
@@ -6,8 +6,6 @@ const path = require('path')
 const fs = require('fs')
 
 const settings = require(path.join(__dirname, 'env.json'))
-const patchDate = require(path.join(__dirname, '..', 'patchDate.json'))['patchDate']
-
 
 let bot = new MWBot()
 
@@ -28,31 +26,31 @@ function askToUpload(array, count, type) {
   bot.read(articleName).then(article => {
     // Exists, check gen date & compare against git latest git commit.
     let articleText = article.query.pages[Object.keys(article.query.pages)[0]]['revisions'] ? article.query.pages[Object.keys(article.query.pages)[0]]['revisions'][0]['*'] : undefined
+    exec(`git log -1 --format=%cd "${path.join(__dirname, '..', 'Patch', type, filename.split('.')[0])}.json"`, (err, stdout, stderr) => {
     if (articleText) {
       let genString = articleText.match(/<!-- ALL LINES ABOVE ARE AUTOMATED[\w;: ,0-9]+ -->/) ? articleText.match(/<!-- ALL LINES ABOVE ARE AUTOMATED[\w;: ,0-9]+ -->/)[0].split(';').pop().replace('GENERATION DATE:', '').replace(' -->', '') : undefined
-      exec(`git log -1 --format=%cd "${path.join(__dirname, '..', 'Wiki Templates', 'Monster', filename)}"`, (err, stdout, stderr) => {
         if (!genString || (Date.parse(stdout) > Date.parse(genString))) {
           // There's an update, continue.
           console.log(`(${filename.split('.')[0]}) needs update!\tArticle: ${genString}\tLocal git: ${stdout}`)
-          goAhead(Date.parse(stdout))
+          goAhead({ type }, new Date(stdout).toUTCString())
         } else {
           console.log(`(${filename.split('.')[0]}) is already up to date.\tArticle: ${genString}\tLocal git: ${stdout}`)
           count++
           if (count === array.length) return console.log('Completed!')
           askToUpload(array, count, type)
         }
-      })
-    } else {
-      // goAhead()
-    }
+      } else {
+        goAhead({ type }, new Date(stdout).toUTCString())
+      }
+    })
   })
-  function goAhead(date) {
+  function goAhead(obj, date) {
     ask(`Upload ${filename.split('.')[0]}?`, (err, answer) => {
       switch (answer) {
         case 'yes':
         case 'y':
-          var file = fs.readFileSync(path.join(__dirname, '..', 'Wiki Templates', patchDate, type, filename), 'utf8')
-          if (date) file += `\n\n<!-- ALL LINES ABOVE ARE AUTOMATED, CHANGES DONE ABOVE MAY BE OVERWRITTEN;GENERATION DATE: } -->`
+          var file = fs.readFileSync(path.join(__dirname, '..', 'Wiki Templates', obj.type, filename), 'utf8')
+          if (date) file += `\n\n<!-- ALL LINES ABOVE ARE AUTOMATED, CHANGES DONE ABOVE MAY BE OVERWRITTEN;GENERATION DATE:${date} -->`
           bot.edit(articleName, file, 'Generated with SB-Data bot.').then(response => {
             console.log(response)
             count++
@@ -81,19 +79,19 @@ bot.loginGetEditToken({
     switch (answer) {
       case 'monster':
       case 'mon':
-        var array = fs.readdirSync(path.join(__dirname, '..', 'Wiki Templates', patchDate, 'Monster'))
+        var array = fs.readdirSync(path.join(__dirname, '..', 'Wiki Templates', 'Monster'))
         var count = 0
         askToUpload(array, count, 'Monster')
         break
       case 'item':
       case 'itm':
-        var array = fs.readdirSync(path.join(__dirname, '..', 'Wiki Templates', patchDate, 'ItemDefinition'))
+        var array = fs.readdirSync(path.join(__dirname, '..', 'Wiki Templates', 'ItemDefinition'))
         var count = 0
         askToUpload(array, count, 'ItemDefinition')
         break
       case 'loot':
       case 'lot':
-        var array = fs.readdirSync(path.join(__dirname, '..', 'Wiki Templates', patchDate, 'LootTable'))
+        var array = fs.readdirSync(path.join(__dirname, '..', 'Wiki Templates', 'LootTable'))
         var count = 0
         askToUpload(array, count, 'LootTable')
         break
